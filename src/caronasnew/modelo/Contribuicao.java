7b3e4090992297f22ee2af2a2eeee72e6b689b85
Gabriel <Gabriel@DESKTOP-OAN80AC>
2020-04-10 12:01:03 -0300

    feat: Cria classe Contribuicao

---
+++ src/caronasnew/modelo/Contribuicao.java
@@ -0,0 +1,18 @@
+package caronasnew.modelo;
+
+
+public class Contribuicao{
+
+  
+    private Double valor;
+    
+    
+     public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+    
+}
