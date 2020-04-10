fd8e8fc27ab4a312d256a24760ac6f949adf6c20
Gabriel <Gabriel@DESKTOP-OAN80AC>
2020-04-10 12:04:10 -0300

    feat: Cria métados toString, equals e hascode na classe Contribuicao

--- src/caronasnew/modelo/Contribuicao.java
+++ src/caronasnew/modelo/Contribuicao.java
@@ -1,5 +1,7 @@
 package caronasnew.modelo;
 
+import java.util.Objects;
+
 
 public class Contribuicao{
 
@@ -13,6 +15,35 @@ public class Contribuicao{
 
     public void setValor(Double valor) {
         this.valor = valor;
+    }
+
+    public int hashCode() {
+        int hash = 5;
+        hash = 89 * hash + Objects.hashCode(this.valor);
+        return hash;
+    }
+
+ 
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Contribuicao other = (Contribuicao) obj;
+        if (!Objects.equals(this.valor, other.valor)) {
+            return false;
+        }
+        return true;
+    }
+
+ 
+    public String toString() {
+        return "Contribuicao{" + "valor=" + valor + '}';
     }
     
 }
