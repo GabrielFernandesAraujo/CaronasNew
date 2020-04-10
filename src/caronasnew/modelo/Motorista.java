bdf47d4f566226a104af2d12d55161881769fe9e
miguelcosta <miguelcosta@LAPTOP-F1IKDJKD>
2020-04-10 20:58:22 -0300

    Feat: Criar classe Motorista com os atributos que são dados de veiculo e CNH do usuário Além disso, fazer emcapsulamento dos atributos que pertence a classe Motorista.

---
+++ src/caronasnew/modelo/Motorista.java
@@ -0,0 +1,195 @@
+
+package caronasnew.modelo;
+
+public class Motorista {
+     private String nomeTitular;
+     private String renavam;
+     private Integer exercico;
+     private Integer via;
+     private String rntrc;
+     private String cpfTitular;
+     private String placa; 
+     private String placaAntUf;
+     private String chassi;
+     private String especieTipo;
+     private String marcaModelo;
+     private Integer anoModelo;
+     private Integer anoFabricacao;
+     private String cap;
+     private String cartegria;
+     private String cor;
+     private String ipva;
+     private String dpvat;
+     private String local;
+     private Integer dataEmissao;
+     private Integer cnhMotorista;
+     
+    public String getNomeTitular() {
+        return nomeTitular;
+    }
+
+    public void setNomeTitular(String nomeTitular) {
+        this.nomeTitular = nomeTitular;
+    }
+
+    public String getRenavam() {
+        return renavam;
+    }
+
+    public void setRenavam(String renavam) {
+        this.renavam = renavam;
+    }
+
+    public Integer getExercico() {
+        return exercico;
+    }
+
+    public void setExercico(Integer exercico) {
+        this.exercico = exercico;
+    }
+
+    public Integer getVia() {
+        return via;
+    }
+
+    public void setVia(Integer via) {
+        this.via = via;
+    }
+
+    public String getRntrc() {
+        return rntrc;
+    }
+
+    public void setRntrc(String rntrc) {
+        this.rntrc = rntrc;
+    }
+
+    public String getCpfTitular() {
+        return cpfTitular;
+    }
+
+    public void setCpfTitular(String cpfTitular) {
+        this.cpfTitular = cpfTitular;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getPlacaAntUf() {
+        return placaAntUf;
+    }
+
+    public void setPlacaAntUf(String placaAntUf) {
+        this.placaAntUf = placaAntUf;
+    }
+
+    public String getChassi() {
+        return chassi;
+    }
+
+    public void setChassi(String chassi) {
+        this.chassi = chassi;
+    }
+
+    public String getEspecieTipo() {
+        return especieTipo;
+    }
+
+    public void setEspecieTipo(String especieTipo) {
+        this.especieTipo = especieTipo;
+    }
+
+    public String getMarcaModelo() {
+        return marcaModelo;
+    }
+
+    public void setMarcaModelo(String marcaModelo) {
+        this.marcaModelo = marcaModelo;
+    }
+
+    public Integer getAnoModelo() {
+        return anoModelo;
+    }
+
+    public void setAnoModelo(Integer anoModelo) {
+        this.anoModelo = anoModelo;
+    }
+
+    public Integer getAnoFabricacao() {
+        return anoFabricacao;
+    }
+
+    public void setAnoFabricacao(Integer anoFabricacao) {
+        this.anoFabricacao = anoFabricacao;
+    }
+
+    public String getCap() {
+        return cap;
+    }
+
+    public void setCap(String cap) {
+        this.cap = cap;
+    }
+
+    public String getCartegria() {
+        return cartegria;
+    }
+
+    public void setCartegria(String cartegria) {
+        this.cartegria = cartegria;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getIpva() {
+        return ipva;
+    }
+
+    public void setIpva(String ipva) {
+        this.ipva = ipva;
+    }
+
+    public String getDpvat() {
+        return dpvat;
+    }
+
+    public void setDpvat(String dpvat) {
+        this.dpvat = dpvat;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public Integer getDataEmissao() {
+        return dataEmissao;
+    }
+
+    public void setDataEmissao(Integer dataEmissao) {
+        this.dataEmissao = dataEmissao;
+    }
+
+    public Integer getCnhMotorista() {
+        return cnhMotorista;
+    }
+
+    public void setCnhMotorista(Integer cnhMotorista) {
+        this.cnhMotorista = cnhMotorista;
+    }
+    
+}
