4f454ab08870672cae9666d7871fe1f7e2d4f60a
miguelcosta <miguelcosta@LAPTOP-F1IKDJKD>
2020-05-15 18:31:46 -0300

    Feat: Criar o método toString na classe Corrida e fazer comentário

---
+++ src/br/caronasNew/relacionamentoUsuario/Login.java
@@ -0,0 +1,38 @@
+package br.caronasNew.relacionamentoUsuario;
+
+/** Essa classe é de Login, foi criado os atributos, emcapsulamento 
+ dos atributos e metro construstor.
+ *
+ * @author miguelcosta
+ */
+public class Login {
+    private String nomeUsuario;
+    private String senha;
+    
+     /** 
+        METRO CONSTRUTOR
+      
+      */
+
+    public Login(String nomeUsuario, String senha) {
+        this.nomeUsuario = nomeUsuario;
+        this.senha = senha;
+    }
+   
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+}
